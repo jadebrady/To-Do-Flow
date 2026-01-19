4593a786992b20fcefbbb698a43aabb9669c7ad8
Jade <jadebrady1300@gmail.com>
2026-01-19 10:07:15 +1000

    Created and connected to MySQL database

---
+++ ToDoApp.Data/query.sql
@@ -0,0 +1,13 @@
+-- CREATE TABLE ToDoList (
+--     Id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     Title TEXT NOT NULL,
+--     CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     DueDate TEXT,
+--     IsCompleted INTEGER DEFAULT 0
+-- );
+
+INSERT INTO ToDoList (Title, DueDate) VALUES ('Buy groceries', '2024-07-01');
+INSERT INTO ToDoList (Title, DueDate) VALUES ('Finish project report', '2024-07-05');
+INSERT INTO ToDoList (Title, DueDate) VALUES ('Call the bank', '2024-06-30');
+
+SELECT * FROM ToDoList WHERE IsCompleted = 0 ORDER BY DueDate ASC;
